9755d651c989112f9edd399ecc5e0138708d42bf
Qinlan Weng <QWeng@slb.com>
2022-10-27 17:47:28 +0800

    feat: add person class with introduce

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -6,10 +6,16 @@ namespace OOStepByStepTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_return_message_when_introduce_oneself_given_name_Tom_and_age_21()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            //given
+            Person person = new Person("Tom", 21);
+
+            //when
+            var message = person.Introduce();
+
+            //then
+            Assert.Equal("My name is Tom. I am 21 years old.", message);
         }
     }
 }
