2baa1239d1d848d8f99e48eb33504a990f82d0e9
Zejie Yao <ZYao3@slb.com>
2022-10-27 17:39:39 +0800

    feat: add Should_return_name_and_age_when_given_person_with_name_and_age function and test

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOStepByStepTest
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Speak()
+        {
+            return $"My name is {this.name}. I am {this.age} years old.";
+        }
+    }
+}
---
+++ OOStepByStepTest/StepByStepTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OOStepByStepTest
+{
+    public class StepByStepTest
+    {
+        [Fact]
+        public void Should_return_name_and_age_when_given_person_with_name_and_age()
+        {
+            //given
+            Person person = new Person("Tom", 21);
+            //when
+            string res = person.Speak();
+            //then
+            Assert.Equal("My name is Tom. I am 21 years old.", res);
+        }
+    }
+}
