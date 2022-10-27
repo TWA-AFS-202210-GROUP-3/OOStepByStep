e4030eddc580261c0ef54373a955d9cab69c2203
Zejie Yao <ZYao3@slb.com>
2022-10-27 17:49:16 +0800

    feat: add Should_return_name_and_age_and_type_when_given_student_with_name_and_age function and test

---
+++ OOStepByStep/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace OOStepByStepTest
+{
+    public class Student : Person
+    {
+        private string name;
+        private int age;
+
+        public Student(string name, int age) : base(name, age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public string Speak()
+        {
+            return $"My name is {this.Name}. I am {this.Age} years old. I am a student.";
+        }
+    }
+}
--- OOStepByStepTest/StepByStepTest.cs
+++ OOStepByStepTest/StepByStepTest.cs
@@ -19,5 +19,16 @@ namespace OOStepByStepTest
             //then
             Assert.Equal("My name is Tom. I am 21 years old.", res);
         }
+
+        [Fact]
+        public void Should_return_name_and_age_and_type_when_given_student_with_name_and_age()
+        {
+            //given
+            Student student = new Student("Tom", 18);
+            //when
+            string res = student.Speak();
+            //then
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student.", res);
+        }
     }
 }
