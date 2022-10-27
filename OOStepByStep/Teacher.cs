a0b03f32fddf9de646ef745b588b0a5281c7b876
Fengjun Ma <FMa6@slb.com>
2022-10-27 17:55:39 +0800

    feature: add teacher and student to inherit person

---
+++ OOStepByStep/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace OOStepByStep
+{
+    public class Teacher : Person
+    {
+        public Teacher(string name, int age) : base(name, age)
+        {
+        }
+
+        public override string Introduce()
+        {
+            return base.Introduce() + " I am a teacher.";
+        }
+    }
+}
--- OOStepByStepTest/Test.cs
+++ OOStepByStepTest/Test.cs
@@ -15,5 +15,27 @@ namespace OOStepByStepTest
             //then
             Assert.Equal("My name is Tom. I am 21 years old.", introduceMessage);
         }
+
+        [Fact]
+        public void Should_return_introduce_message_when_given_a_teacher()
+        {
+            //given
+            Teacher teacher = new Teacher("Amy", 30);
+            //when
+            string introduceMessage = teacher.Introduce();
+            //then
+            Assert.Equal("My name is Amy. I am 30 years old. I am a teacher.", introduceMessage);
+        }
+
+        [Fact]
+        public void Should_return_introduce_message_when_given_a_student()
+        {
+            //given
+            Student student = new Student("Tom", 18);
+            //when
+            string introduceMessage = student.Introduce();
+            //then
+            Assert.Equal("My name is Tom. I am 18 years old. I am a student.", introduceMessage);
+        }
     }
 }
